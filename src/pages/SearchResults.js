ae523f3162a08d9d86d6a15077f583a6eceee5f0
halait <40286718+halait@users.noreply.github.com>
2021-09-18 17:22:43 -0400

    searchresults changes

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,14 +1,22 @@
-import { getJournalist } from "../service.js";
+import { searchJournalists } from "../service.js";
 import styles from "./SearchResults.module.css";
 
 import List from "../components/SearchResults/List";
 
 function SearchReults() {
+  // TODO diplay jounalist stuff
   return (
     <div className={styles.listStyles}>
       <List />
     </div>
   );
+}
+
+async function getSearchResults() {
+  const query = (new URLSearchParams(window.location.search)).get('q');
+  let results = await searchJournalists(query);
+  console.log(results);
+  return results;
 }
 
 export default SearchReults;
