54859a404cda1b2d35c89ae66a24834d4c7becd7
Karolina <karolinaswislocki@hotmail.com>
2021-09-18 15:53:54 -0400

    final touches search results

--- src/components/SearchResults/List.js
+++ src/components/SearchResults/List.js
@@ -13,7 +13,13 @@ const useStyles = makeStyles({
   imgStyles: {
     borderRadius: "50%",
   },
-  cardStyles: { width: "200px" },
+  nameStyles: {
+    fontFamily: "Poppins",
+    fontWeight: "600",
+    fontSize: "18px",
+    margin: "0px 0px 10px 10px",
+  },
+  rating: { margin: "0px 0px 15px 10px", fontFamily: "Poppins" },
 });
 
 export default function FolderList() {
@@ -21,7 +27,14 @@ export default function FolderList() {
   return (
     <Card>
       <h1 className={styles.headerStyles}>Search Results</h1>
-      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      <List
+        sx={{
+          width: "100%",
+          maxWidth: "100%",
+          bgcolor: "background.paper",
+          padding: "10px",
+        }}
+      >
         <ListItem>
           <ListItemAvatar>
             <img
@@ -30,14 +43,44 @@ export default function FolderList() {
               width="100px"
             />
           </ListItemAvatar>
-          <Typography variant="h5" component="div">
-            Elon Musk
-          </Typography>
-          <Typography variant="body2" component="div" color="text.secondary">
-            RATING
-          </Typography>
+          <div>
+            <Typography variant="h5" component="div" class={classes.nameStyles}>
+              Elon Musk
+            </Typography>
+            <Typography
+              variant="body2"
+              component="div"
+              color="text.secondary"
+              class={classes.rating}
+            >
+              RATING
+            </Typography>
+          </div>
         </ListItem>
+
         <Divider />
+        <ListItem>
+          <ListItemAvatar>
+            <img
+              className={classes.imgStyles}
+              src="https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTc5OTk2ODUyMTMxNzM0ODcy/gettyimages-1229892983-square.jpg"
+              width="100px"
+            />
+          </ListItemAvatar>
+          <div>
+            <Typography variant="h5" component="div" class={classes.nameStyles}>
+              Elon Musk
+            </Typography>
+            <Typography
+              variant="body2"
+              component="div"
+              color="text.secondary"
+              class={classes.rating}
+            >
+              RATING
+            </Typography>
+          </div>
+        </ListItem>
       </List>
     </Card>
   );
