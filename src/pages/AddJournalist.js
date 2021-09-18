85aadcebcd8b8bacb0432d08806bebf933a98873
Christopher Kotsopoulos <kotsopoc@uoguelph.ca>
2021-09-18 15:08:26 -0400

    Display picture null

--- src/pages/AddJournalist.js
+++ src/pages/AddJournalist.js
@@ -13,7 +13,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 const theme = createTheme();
 
 function AddJournalist(){
-  const [displayPicture, setDisplayPicture] = React.useState(defaultProfile);
+  const [displayPicture, setDisplayPicture] = React.useState(null);
   const [preview, setPreview] = React.useState(defaultProfile);
 
   const handleSubmit = (event) => {
