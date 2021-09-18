06a5b4ab7e1d97243c58527f309314bbf4d13272
Christopher Kotsopoulos <kotsopoc@uoguelph.ca>
2021-09-18 16:38:41 -0400

    Signin button fix

--- src/components/SignInButton.js
+++ src/components/SignInButton.js
@@ -1,6 +1,9 @@
 import * as React from "react";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import { makeStyles } from "@mui/styles";
 import { userManager } from '../service.js';
 
@@ -16,18 +19,58 @@ const useStyles = makeStyles({
 
 export default function SignInButton() {
   const classes = useStyles();
-  return (
+  const [username, setUsername] = React.useState('');
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleProfile = () => {
+    //href to the user profile here
+    setAnchorEl(null);
+  };
+
+  userManager.subscribe((user) => {
+    if(user){
+      setUsername(user.name);
+    }
+  });
+
+  return username === '' ? (
     <Stack spacing={2} direction="row">
-      {/* <Button variant="contained">Contained</Button> */}
       <Button href="/signin" variant="contained" className={classes.buttonStyle}>
         Sign In
       </Button>
     </Stack>
+  ) : (
+    <Stack spacing={2} direction="row">
+      <Button variant="contained" className={classes.buttonStyle} onClick={handleClick}>
+        <Typography>
+          {username[0]}
+        </Typography>
+      </Button>
+      <Menu
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <MenuItem onClick={handleProfile}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </Stack>
   );
 }
-
-
-userManager.subscribe((user) => {
-  // TODO change state based on user
-  console.log(user);
-});
